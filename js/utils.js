4f6e9efe7cf6e7e75c4f2b9b638cc6fb154208db
Travor Liu <travor_lzh@outlook.com>
2017-11-16 11:35:49 -0500

    Updated JavaScript

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,3 @@
 function init404(){
-	document.getElementById("msg").innerText="The requested URL "+getCurrentPath()+" was not found on this server.";
-}
-function getCurrentPath(){
-	return window.location.href;
+	document.getElementById("msg").innerText="The requested URL "+document.location.pathname+" was not found on this server.";
 }
