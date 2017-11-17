0c54f76ccbc6f15cba6f3c4a7494d07e6adf8f24
Travor Liu <travor_lzh@outlook.com>
2017-11-17 09:23:55 -0500

    Added directory assets

---
+++ assets/js/utils.js
@@ -0,0 +1,10 @@
+var p404={
+	init:function(){
+		document.getElementById("msg").innerText="The requested URL "+document.location.pathname+" was not found on this server.";
+	}
+};
+var index={
+	init:function(){
+		console.info("Welcome");
+	}
+};
